97ccdc6cb7c0111b9a8f112b9df530da0fe14b1c
ReganRyanNZ <regan.ryan.nz@gmail.com>
2016-09-27 17:29:16 +1300

    Add files via upload

--- big_image_tiler.sh
+++ big_image_tiler.sh
@@ -8,21 +8,28 @@ if (( $# != 2 )); then
     exit 1
 fi
 
+size=256
 name=$2
 mkdir $name
-convert -monitor -limit area 2mb $1 myLargeImg.mpc
+convert  -gravity NorthWest -monitor -limit area 2mb $1 myLargeImg.mpc
 src=myLargeImg.mpc
 width=`identify -format %w $src`
-limit=$[$width / 256]
-echo "count = $limit * $limit = "$((limit * limit))" tiles"
-limit=$((limit-1))
-for x in `seq 0 $limit`; do
-  for y in `seq 0 $limit`; do
-    tile=$name/$name\_$x\_$y.png
+height=`identify -format %h $src`
+echo "width: $width"
+echo "height: $height"
+w_limit=$[$width / $size]
+h_limit=$[$height / $size]
+echo "count = $w_limit * $h_limit = "$((w_limit * h_limit))" tiles"
+w_limit=$((w_limit))
+h_limit=$((h_limit))
+for x in `seq 0 $w_limit`; do
+  for y in `seq 0 $h_limit`; do
+    tile=$name/$name\_$x\_$y.jpg
     echo -n $tile
-    w=$((x * 256))
-    h=$((y * 256))
-    convert -debug cache -monitor $src -crop 256x256+$w+$h $tile
+    w=$((x * size))
+    h=$((y * size))
+    convert -monitor $src -gravity NorthWest -crop $size\x$size+$w+$h $tile
+    # convert -debug cache -monitor $src -gravity NorthWest -crop $size\x$size+$w+$h $tile
   done
 done
 rm myLargeImg.*
